0510ddd05c703899c928dbab5f1355f8286355fa
dario2434 <113589458+dario2434@users.noreply.github.com>
2025-02-12 15:18:38 +0000

    Update UML.py
    Arreglando algunos errores, y ahora llevándolo a una prueba

--- UML.py
+++ UML.py
@@ -1,19 +1,23 @@
+#Creando Base de Datos
+database = []
 # Código en Python (Signin)
 def signin(user):
   user = {
-    userId = '55_323_45_DC'
-    username = 'Pepe'
-    password = '1234Pepe1'
-    email = 'xxxx@email.com'
+    'userId' = '55_323_405_DC'
+    'username' = 'Pepe'
+    'password' = '1234Pepe1'
+    'email' = 'example@email.com'
     }
   validate_user = False
-  for user in database:
-    if database[user] == database[user - 1]:
-      return validate_user
-    elif isinstance(user.values(), str) and 8 >= len(user[username]) <= 10:
-      database.extend(user)
+
+  if user in database:
+    return False
+    
+  if isinstance(user.values(), str) and 8 >= len(user[username]) <= 10:
+      database.append(user)
       validate_user = True
-  return user
+  #usuario validao
+  return validate_user 
 
 #def validate_credentials(user):
  # if isinstance(user.values(), str) and 8 >= len(user[username]) <= 10:
