db5a1f3f79e746605ab1a7783e60280cf32f96c5
Efrén Pérez Marrero <ab1c1uk9@gmail.com>
2025-02-14 17:58:51 +0000

    eliminación del código comentado desfasado UML.py

--- UML.py
+++ UML.py
@@ -20,20 +20,3 @@ def signin(user: dict[str, str]) -> bool:
         database.append(user)
         validate_user = True #usuario validado
     return validate_user 
-
-#def validate_credentials(user):
- # if isinstance(user.values(), str) and 8 >= len(user[username]) <= 10:
-  #  return True
-  # else:
-  #  return False
-
-# def get_details(user):
-  # database.extend(user)
-  # return user
-
-# def add_user(user):
-  # for user in database:
-    # if database[user] == database[user - 1]:
-      # return False
-    # else:
-      # return True
