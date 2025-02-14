84a2a429b8e19284d40fd1188061f5f2f990f979
Efrén Pérez Marrero <ab1c1uk9@gmail.com>
2025-02-14 18:09:23 +0000

    Agregación de la función login UML.py

--- UML.py
+++ UML.py
@@ -19,4 +19,20 @@ def signin(user: dict[str, str]) -> bool:
     if all(isinstance(value, str) for value in user.values()) and 4 <= len(user["username"]) <= 20: 
         database.append(user)
         validate_user = True #usuario validado
-    return validate_user 
+    return validate_user
+    
+# El usuario ya ha sido registrado en la base de datos
+database = [
+    {"userId": "55_223_405_DC", "username": "Pepe Morales", "password": "1234Pepe1", "email": "example@email.com"}
+]
+
+# Código en Python (Login)
+def login(user: dict[str, str]) -> bool:
+    # Verificamos, por ejemplo, si el usuario y la contraseña coinciden con alguno de los usuarios ya almacenados
+    for existing_user in database:
+        if existing_user["username"] == user["username"] and existing_user["password"] == user["password"]:
+            return True  # Login exitoso
+    return False  # Login fallido. Esto ocurre porque no encuentra ninguna coincidencia
+
+# Código en Python (Logout)
+def logout():
