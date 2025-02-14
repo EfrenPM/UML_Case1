41a5b576381c4d354a3b2f3ac01f1d3d368b9a8e
Efrén Pérez Marrero <ab1c1uk9@gmail.com>
2025-02-14 11:15:33 +0000

    Pequeñas correciones de código UML.py

--- UML.py
+++ UML.py
@@ -1,22 +1,24 @@
-#Creando Base de Datos
-database = []
+# Creando la Base de Datos
+database = [] # En el ejemplo está vacía, pero realmente hay miles de usuarios registrados/almacenados
 # Código en Python (Signin)
-def signin():
+def signin(user: dict[str, str]) -> bool:
     user = {
-        "userId" : "55_323_405_DC",
-        "username" : "Pepe",
+        "userId" : "55_223_405_DC",
+        "username" : "Pepe Morales",
         "password" : "1234Pepe1",
         "email" : "example@email.com"
     }
     validate_user = False
-
-    if user in database:
-        return False
     
-    if isinstance(user.values(), str) and 8 >= len(user["username"]) <= 10:
+    # Verificamos si existe ya un usuario
+    for existing_user in database:
+        if any(value in existing_user.values() for value in user.values()):
+            return False
+            
+    # Confirmarmos que todos los valores del usuario sean strings y que el nombre de usuario tenga una longitud válida
+    if all(isinstance(value, str) for value in user.values()) and 4 <= len(user["username"]) <= 20: 
         database.append(user)
-  #usuario validao
-        validate_user = True
+        validate_user = True #usuario validado
     return validate_user 
 
 #def validate_credentials(user):
